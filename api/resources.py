8881d2bd15f9aedcfbea39830b9a073b2d9ed448
gautamamber <ambergautam1@gmail.com>
2018-07-21 22:11:57 +0530

    Authorization added

--- api/resources.py
+++ api/resources.py
@@ -1,8 +1,10 @@
 from tastypie.resources import ModelResource
 from api.models import Note
-
+from tastypie.authorization import Authorization
 class NoteResource(ModelResource):
 	class Meta:
 		queryset = Note.objects.all()
 		resource_name = 'note'
-		
+		authorization = Authorization()
+		#limiting fields 
+		fields = ['title' , 'body']
